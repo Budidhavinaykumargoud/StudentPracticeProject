8e6017e53334adbf8974bc66c9d0fb2c3d0f003f
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2023-11-27 17:18:12 +0530

    Student

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr;
+            try
+            {
+                string fpath = "C:\\Users\\User\\OneDrive\\Desktop\\simplilearn\\Student.txt";
+                sr = new StreamReader(fpath);
+                string text = " ";
+                while ((text = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(text);
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
